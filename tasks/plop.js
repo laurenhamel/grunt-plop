582bb0c8f3f0af208689d8a95bc341001be4b446
Lauren Hamel <lauren.hamel@outlook.com>
2019-03-04 10:44:57 -0500

    Removes `plop` as a dependency
    Dependencies are no longer installed alongside this package. Requires that the `plop` package be installed separately. This places control  over versioning dependencies back in the hands of the end user.

--- tasks/plop.js
+++ tasks/plop.js
@@ -16,6 +16,24 @@ module.exports = (grunt) => {
     // Make asynchronous.
     const done = this.async();
     
+    // Verify that Plop is available.
+    try { require.resolve('plop'); }
+  
+    // Otherwise, throw an error.
+    catch(error) {
+      
+      // Alert the user that Plop could not be found.
+      grunt.log.error('Plop could not be found.',
+                      'Run `npm i plop --save-dev`, then try again.');
+      
+      // Exit the Grunt task.
+      done();
+      
+      // Stop running.
+      return;
+      
+    }
+    
     // Run Plop.
     grunt.util.spawn({
       cmd: require.resolve('plop'),
